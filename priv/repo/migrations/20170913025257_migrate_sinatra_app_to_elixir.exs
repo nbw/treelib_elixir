01c44302b91e5bacfe97d1607db5d149c6893fec
Nathan Willson <willson.b.nathan@gmail.com>
2017-10-14 21:04:17 -0700

    Create table migratiosn for all the tables from the old Sinatra app

---
+++ priv/repo/migrations/20170913025257_migrate_sinatra_app_to_elixir.exs
@@ -0,0 +1,75 @@
+defmodule Treelib.Repo.Migrations.MigrateSinatraAppToElixir do
+  use Ecto.Migration
+
+  def change do
+  	# %%%%% USERS %%%%%%%
+  	create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :pw_hash, :string
+      add :create_date, :naive_datetime
+      add :disable_date, :naive_datetime
+      add :enabled, :boolean, default: true
+      add :admin_level, :integer, default: 0
+    end
+
+    create unique_index(:users, [:email])
+
+    # %%%%% PHOTO ALBUMS %%%%%%%
+  	create table(:photo_albums) do
+      add :photoset_id, :integer
+      add :name, :string
+      add :last_updated, :naive_datetime
+    end
+
+    # %%%%% PHOTOS %%%%%%%
+  	create table(:photos) do
+      add :flickr_id, :integer
+      add :photoset_id, :integer
+      add :farm, :integer
+      add :secret, :string
+      add :server, :integer
+      add :name, :string
+      add :description, :string
+      add :credit, :string
+      add :disable_date, :naive_datetime
+    end
+
+    # %%%%% FAMILIES %%%%%%%
+  	create table(:families) do
+      add :name, :string
+      add :common_name, :string, default: ""
+      add :description, :string
+      add :enabled, :boolean, default: true
+    end
+
+    # %%%%% GENERA %%%%%%%
+  	create table(:genera) do
+      add :name, :string
+      add :common_name, :string, default: ""
+      add :description, :string
+      add :fam_id, references(:families)
+      add :enabled, :boolean, default: true
+    end
+
+    # %%%%% SPECIES %%%%%%%
+  	create table(:species) do
+      add :name, :string
+      add :common_name, :string, default: ""
+      add :description, :string
+      add :genus_id, references(:genera)
+      add :album_id, references(:photo_albums)
+      add :enabled, :boolean, default: true
+    end
+
+  	# %%%%% SPECIES LINKS %%%%%%%
+		create table(:species_links) do
+    	add :species_id, references(:species)
+    	add :name, :string
+    	add :url, :string
+
+  		timestamps()
+  	end
+
+  end
+end
