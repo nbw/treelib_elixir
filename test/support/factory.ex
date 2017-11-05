975a9c0a6cdeaaa1bc74c2a6cdc3b6f3a8f15c00
Nathan Willson <willson.b.nathan@gmail.com>
2017-11-05 14:33:58 -0800

    factory updated with PhotoAlbum

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -6,6 +6,7 @@ defmodule Treelib.Factory do
   use ExMachina.Ecto, repo: Treelib.Repo
 
   alias Treelib.UserManager.User
+  alias Treelib.PhotoManager.PhotoAlbum
 
   def user_factory do
     %User{
@@ -16,6 +17,14 @@ defmodule Treelib.Factory do
       # password_confirmation: "password",
       admin_level:  0,
       enabled: true,
+    }
+  end
+
+  def photo_album_factory do
+    %PhotoAlbum{
+      photoset_id: 0,
+      name: "Pine",
+      last_updated: ""
     }
   end
 end
