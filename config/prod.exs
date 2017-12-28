d4bd8574ff63504125a4ea7813d50ceb80977a2c
Nathan Willson <willson.b.nathan@gmail.com>
2017-12-28 15:56:09 -0800

    added an ENV to set the ecto/postgrex timeout time * grabbed from [fix/photo_update_timeout]

--- config/prod.exs
+++ config/prod.exs
@@ -58,7 +58,8 @@ config :treelib, Treelib.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: true,
+  timeout: String.to_integer(System.get_env("DATABASE_TIMEOUT") || 15_000)
 
 # Flickr
 config :treelib, 
