2bbc5d9bdcf9bcb2f640a1ded7ec761e5506631b
Nathan Willson <willson.b.nathan@gmail.com>
2018-01-04 22:19:17 -0800

    REGISTRATION_ENABLE flag enabled

--- lib/treelib_web/controllers/registration_controller.ex
+++ lib/treelib_web/controllers/registration_controller.ex
@@ -5,20 +5,28 @@ defmodule TreelibWeb.RegistrationController do
   alias Treelib.UserManager.User
 
   def new(conn, _params) do
-    changeset = User.registration_changeset(%User{}, %{})
-    render conn, "new.html", changeset: changeset
+    with {:ok, "true"} <- Map.fetch(System.get_env(), "REGISTRATION_ENABLED") do
+      changeset = User.registration_changeset(%User{}, %{})
+      render conn, "new.html", changeset: changeset
+    else
+      _ -> redirect conn, to: "/"
+    end
   end
 
   def create(conn, %{"user" => params}) do
-    case UserManager.register_admin_user(params) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "Successfully registered a new account.")
-        |> put_session(:current_user_id, user.id)
-        |> configure_session(renew: true)
-        |> redirect(to: "/login")
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render conn, "new.html", changeset: changeset
+    with {:ok, "true"} <- Map.fetch!(System.get_env(), "REGISTRATION_ENABLED") do
+      case UserManager.register_admin_user(params) do
+        {:ok, user} ->
+          conn
+          |> put_flash(:info, "Successfully registered a new account.")
+          |> put_session(:current_user_id, user.id)
+          |> configure_session(renew: true)
+          |> redirect(to: "/login")
+        {:error, %Ecto.Changeset{} = changeset} ->
+          render conn, "new.html", changeset: changeset
+      end
+    else
+      _ -> redirect conn, to: "/"
     end
   end
 end
