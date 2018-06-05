3e572871cb6e5fa834fbbf1b28bb974cc07ea51b
Nathan Willson <willson.b.nathan@gmail.com>
2018-06-04 21:47:24 -0700

    cast envar to int

--- lib/treelib/photo_manager/photo_checker.ex
+++ lib/treelib/photo_manager/photo_checker.ex
@@ -14,7 +14,7 @@ defmodule Treelib.PhotoManager.PhotoChecker   do
   end
 
   def update do
-    GenServer.call(:photo_checker, :update, System.get_env("DATABASE_TIMEOUT"))
+    GenServer.call(:photo_checker, :update, String.to_integer(System.get_env("DATABASE_TIMEOUT")) || 30000)
   end
 
   def handle_call(:update, _from, state) do
