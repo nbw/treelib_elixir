f78c622fbe95cb8685ff620ee030e2234c474c8d
Nathan Willson <willson.b.nathan@gmail.com>
2019-01-05 19:43:46 -0800

    Rename contrib test to admin contrib test

--- test/treelib_web/controllers/admin_contributor_controller_test.exs
+++ test/treelib_web/controllers/admin_contributor_controller_test.exs
@@ -1,4 +1,4 @@
-defmodule TreelibWeb.ContributorControllerTest do
+defmodule TreelibWeb.AdminContributorControllerTest do
   use TreelibWeb.ConnCase
 
   alias Treelib.Contributions
@@ -9,36 +9,37 @@ defmodule TreelibWeb.ContributorControllerTest do
 
   def fixture(:contributor) do
     {:ok, contributor} = Contributions.create_contributor(@create_attrs)
+
     contributor
   end
 
   describe "index" do
     test "lists all contributors", %{conn: conn} do
-      conn = get(conn, Routes.contributor_path(conn, :index))
+      conn = get(conn, Routes.admin_contributor_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Contributors"
     end
   end
 
   describe "new contributor" do
     test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.contributor_path(conn, :new))
+      conn = get(conn, Routes.admin_contributor_path(conn, :new))
       assert html_response(conn, 200) =~ "New Contributor"
     end
   end
 
   describe "create contributor" do
     test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.contributor_path(conn, :create), contributor: @create_attrs)
+      conn = post(conn, Routes.admin_contributor_path(conn, :create), contributor: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.contributor_path(conn, :show, id)
+      assert redirected_to(conn) == Routes.admin_contributor_path(conn, :show, id)
 
-      conn = get(conn, Routes.contributor_path(conn, :show, id))
+      conn = get(conn, Routes.admin_contributor_path(conn, :show, id))
       assert html_response(conn, 200) =~ "Show Contributor"
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.contributor_path(conn, :create), contributor: @invalid_attrs)
+      conn = post(conn, Routes.admin_contributor_path(conn, :create), contributor: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Contributor"
     end
   end
@@ -47,7 +48,7 @@ defmodule TreelibWeb.ContributorControllerTest do
     setup [:create_contributor]
 
     test "renders form for editing chosen contributor", %{conn: conn, contributor: contributor} do
-      conn = get(conn, Routes.contributor_path(conn, :edit, contributor))
+      conn = get(conn, Routes.admin_contributor_path(conn, :edit, contributor))
       assert html_response(conn, 200) =~ "Edit Contributor"
     end
   end
@@ -56,15 +57,15 @@ defmodule TreelibWeb.ContributorControllerTest do
     setup [:create_contributor]
 
     test "redirects when data is valid", %{conn: conn, contributor: contributor} do
-      conn = put(conn, Routes.contributor_path(conn, :update, contributor), contributor: @update_attrs)
-      assert redirected_to(conn) == Routes.contributor_path(conn, :show, contributor)
+      conn = put(conn, Routes.admin_contributor_path(conn, :update, contributor), contributor: @update_attrs)
+      assert redirected_to(conn) == Routes.admin_contributor_path(conn, :show, contributor)
 
-      conn = get(conn, Routes.contributor_path(conn, :show, contributor))
+      conn = get(conn, Routes.admin_contributor_path(conn, :show, contributor))
       assert html_response(conn, 200)
     end
 
     test "renders errors when data is invalid", %{conn: conn, contributor: contributor} do
-      conn = put(conn, Routes.contributor_path(conn, :update, contributor), contributor: @invalid_attrs)
+      conn = put(conn, Routes.admin_contributor_path(conn, :update, contributor), contributor: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Contributor"
     end
   end
@@ -73,10 +74,10 @@ defmodule TreelibWeb.ContributorControllerTest do
     setup [:create_contributor]
 
     test "deletes chosen contributor", %{conn: conn, contributor: contributor} do
-      conn = delete(conn, Routes.contributor_path(conn, :delete, contributor))
-      assert redirected_to(conn) == Routes.contributor_path(conn, :index)
+      conn = delete(conn, Routes.admin_contributor_path(conn, :delete, contributor))
+      assert redirected_to(conn) == Routes.admin_contributor_path(conn, :index)
       assert_error_sent 404, fn ->
-        get(conn, Routes.contributor_path(conn, :show, contributor))
+        get(conn, Routes.admin_contributor_path(conn, :show, contributor))
       end
     end
   end
