82fd132ae2fab49e7159954c980e5218e19e5c00
Nathan Willson <willson.b.nathan@gmail.com>
2019-07-03 20:59:39 +0900

    Admin user seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias Treelib.Repo
+alias Treelib.UserManager.User
+
+# USER
+# Create an admin user with password 123456789
+%User{admin_level: true}
+|> User.admin_registration_changeset(%{
+  email: "admin@treelib.ca",
+  name: "Admin",
+  password: "123456789",
+  password_confirmation: "123456789",
+  admin_level: true,
+  enabled: true
+})
+|> Repo.insert!()
