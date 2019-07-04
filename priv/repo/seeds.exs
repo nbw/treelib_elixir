9c674d9be0f53a71b42c3f48314556536ea8a76f
Nathan Willson <willson.b.nathan@gmail.com>
2019-07-05 07:12:18 +0900

    Add seeds for contributors

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -22,6 +22,7 @@ alias Treelib.PhotoManager.{
   PhotoAlbum,
   Photo
 }
+alias Treelib.Contributions.Contributor
 
 defmodule Treelib.DbSeeder do
 
@@ -29,7 +30,7 @@ defmodule Treelib.DbSeeder do
   Inserts a family.
   Inserts genera into that family.
   Inserts species in to those genera,
-    with a photo album that has photos.
+  with a photo album that has photos.
   """
   def insert_tree(
     num_families \\ 1,
@@ -37,104 +38,129 @@ defmodule Treelib.DbSeeder do
     num_species \\ 1
   ) do
 
-      # Photo Album and Photos for species
-      photo_album = insert_album();
-      (1..25)
-      |> Enum.each(fn _ ->
-        insert_photo()
-      end)
+    # Photo Album and Photos for species
+    photo_album = insert_album();
+    (1..25)
+    |> Enum.each(fn _ ->
+      insert_photo()
+    end)
 
-      (1..num_families)
-      |> Enum.each(fn i ->
-        family = insert_family(i)
+    (1..num_families)
+    |> Enum.each(fn i ->
+      family = insert_family(i)
 
-        (1..num_genera)
-        |> Enum.each(fn j ->
-          genus = insert_genus(j, family.id)
+      (1..num_genera)
+      |> Enum.each(fn j ->
+        genus = insert_genus(j, family.id)
 
-          (1..num_species)
-          |> Enum.each(fn h ->
-            insert_species(h, genus.id, photo_album.id)
-          end)
+        (1..num_species)
+        |> Enum.each(fn h ->
+          insert_species(h, genus.id, photo_album.id)
         end)
       end)
-    end
+    end)
 
-    def insert_family(increment) do
-      %Family{}
-      |> Family.changeset(%{
-        name: "#{lorem_name()} #{increment} ",
-        common_name: "#{lorem_name()} ",
-        description: "#{lorem_description()}"
-      })
-      |> Repo.insert!()
-    end
+    species = Species.active |> Repo.all
+    (1..15)
+    |> Enum.each(fn c ->
+      species_ids =
+        Enum.take_random(species, c)
+        |> Enum.map(fn s -> s.id end)
 
-    def insert_genus(increment, family_id) do
-      %Genus{}
-      |> Genus.changeset(%{
-        name: "#{lorem_name()} #{increment} ",
-        common_name: lorem_name(),
-        description: lorem_description(),
-        fam_id: family_id
-      })
-      |> Repo.insert!()
-    end
+      insert_contributor(species_ids)
+    end)
+  end
 
-    def insert_species(increment, genus_id, photo_album_id) do
-      %Species{}
-      |> Species.changeset(%{
-        name: "#{lorem_name()} #{increment} ",
-        common_name: lorem_name(),
-        description: lorem_description(),
-        genus_id: genus_id,
-        album_id: photo_album_id
-      })
-      |> Repo.insert!()
-    end
+  def insert_family(increment) do
+    %Family{}
+    |> Family.changeset(%{
+      name: "#{lorem_name()} #{increment} ",
+      common_name: "#{lorem_name()} ",
+      description: "#{lorem_description()}"
+    })
+    |> Repo.insert!()
+  end
 
-    def insert_album(
-      photoset_id \\ 72157703072517021
-    ) do
-      %PhotoAlbum{}
-      |> PhotoAlbum.changeset(%{
-        name: lorem_name(),
-        photoset_id: photoset_id,
-        last_updated: ~N[2018-06-11 23:00:07.000000Z]
-      })
-      |> Repo.insert!()
-    end
+  def insert_genus(increment, family_id) do
+    %Genus{}
+    |> Genus.changeset(%{
+      name: "#{lorem_name()} #{increment} ",
+      common_name: lorem_name(),
+      description: lorem_description(),
+      fam_id: family_id
+    })
+    |> Repo.insert!()
+  end
 
-    def insert_photo(
-      photoset_id \\ 72157703072517021,
-      flickr_id \\ 34334479214,
-      farm \\ 5,
-      secret \\ "12a854bcbc",
-      server \\ 4274
-    ) do
-      %Photo{}
-      |> Photo.changeset(%{
-        flickr_id: flickr_id,
-        farm: farm,
-        secret: secret,
-        server: server,
-        name: lorem_name(),
-        description: lorem_description(),
-        photoset_id: photoset_id
-      })
-      |> Repo.insert!()
-    end
+  def insert_species(increment, genus_id, photo_album_id) do
+    %Species{}
+    |> Species.changeset(%{
+      name: "#{lorem_name()} #{increment} ",
+      common_name: lorem_name(),
+      description: lorem_description(),
+      genus_id: genus_id,
+      album_id: photo_album_id
+    })
+    |> Repo.insert!()
+  end
 
-    defp lorem_name(range \\ 1..2) do
-      Faker.Lorem.words(range)
-      |> Enum.join(" ")
-      |> String.capitalize()
-    end
+  def insert_album(
+    photoset_id \\ 72157703072517021
+  ) do
+    %PhotoAlbum{}
+    |> PhotoAlbum.changeset(%{
+      name: lorem_name(),
+      photoset_id: photoset_id,
+      last_updated: ~N[2018-06-11 23:00:07.000000Z]
+    })
+    |> Repo.insert!()
+  end
 
-    defp lorem_description(range \\ 1..3) do
-      Faker.Lorem.sentences(range)
-      |> Enum.join(" ")
-    end
+  def insert_photo(
+    photoset_id \\ 72157703072517021,
+    flickr_id \\ 34334479214,
+    farm \\ 5,
+    secret \\ "12a854bcbc",
+    server \\ 4274
+  ) do
+    %Photo{}
+    |> Photo.changeset(%{
+      flickr_id: flickr_id,
+      farm: farm,
+      secret: secret,
+      server: server,
+      name: lorem_name(),
+      description: lorem_description(),
+      photoset_id: photoset_id
+    })
+    |> Repo.insert!()
+  end
+
+  def insert_contributor(species_ids \\ []) do
+    %Contributor{}
+    |> Contributor.changeset(%{
+      first_name: lorem_first_name(),
+      last_name: lorem_last_name(),
+      description: lorem_description(),
+      species_ids: species_ids
+    })
+    |> Repo.insert()
+  end
+
+  defp lorem_first_name, do: Faker.Name.first_name()
+
+  defp lorem_last_name, do: Faker.Name.last_name()
+
+  defp lorem_name(range \\ 1..2) do
+    Faker.Lorem.words(range)
+    |> Enum.join(" ")
+    |> String.capitalize()
+  end
+
+  defp lorem_description(range \\ 1..3) do
+    Faker.Lorem.sentences(range)
+    |> Enum.join(" ")
+  end
 end
 
 # Create an admin user with password 123456789
