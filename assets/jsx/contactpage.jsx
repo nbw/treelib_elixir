1a79063eeda9fe5487993ae4068d270b5eeb267d
Nathan Willson <willson.b.nathan@gmail.com>
2019-11-11 07:32:59 +0900

    Add Contact header to page

--- assets/jsx/contactpage.jsx
+++ assets/jsx/contactpage.jsx
@@ -10,19 +10,20 @@ export default class Contact extends React.Component {
         <BasicNavbar />
         <div className="contactPage">
           <div className="section">
+            <h1>Contact</h1>
             <table className="content">
               <tbody>
                 <tr>
                   <td><label>How you can connect</label></td>
                   <td>
                     <p>
-                      Life is about connecting to other people and finding ways to serve them. 
+                      Life is about connecting to other people and finding ways to serve them.
                     </p>
-                    <p> 
+                    <p>
                       If you have some knowledge of and enjoy photographing trees, connect with us and contribute photographs of new species, improve our shots with your own, introduce great parks you have hiked around.
                     </p>
                     <p>
-                      Become a part of a network of friends who love trees and the outdoors, building the site together.  It is as simple as sending us an email. 
+                      Become a part of a network of friends who love trees and the outdoors, building the site together.  It is as simple as sending us an email.
                     </p>
                   </td>
                 </tr>
