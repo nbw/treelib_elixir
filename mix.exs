0d22ff5642213f6c7d00aaebe6205b333d1cdc99
Nathan Willson <willson.b.nathan@gmail.com>
2019-12-30 09:48:28 -0800

    Upgrade Phoenix

--- mix.exs
+++ mix.exs
@@ -33,13 +33,13 @@ defmodule Treelib.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
+      {:phoenix, "~> 1.4.6"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       # {:mariaex, "~> 0.8.2"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.13.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:plug_cowboy, "~> 2.0"},
