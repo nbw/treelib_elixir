45e01fef79091d66be0f765bbbc05182ad077ab1
Nathan Willson <willson.b.nathan@gmail.com>
2019-12-30 10:18:32 -0800

    Upgrade ecto_sql

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Treelib.Mixfile do
       {:phoenix, "~> 1.4.6"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
+      {:ecto_sql, "~> 3.3.2"},
       {:postgrex, ">= 0.0.0"},
       # {:mariaex, "~> 0.8.2"},
       {:phoenix_html, "~> 2.13.3"},
