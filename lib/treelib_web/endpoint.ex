52e158d11cb7ec9a9b563d28b387f5acd3af8dd8
Nathan Willson <willson.b.nathan@gmail.com>
2020-01-04 12:38:56 -0800

    Upgrade to font awesome 5.12

--- lib/treelib_web/endpoint.ex
+++ lib/treelib_web/endpoint.ex
@@ -10,7 +10,7 @@ defmodule TreelibWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :treelib, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css webfonts fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
