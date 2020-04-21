0da06b50c70539f43ef9a3caf94aabb05458b638
Nathan Willson <willson.b.nathan@gmail.com>
2020-04-21 14:43:53 +0900

    Fix/unlinked albums (#147)
    * Remove progress from npm compile
    
    * Task for linking species to albums that share the same name
    
    Address an issue where albums were unlinked in b/c of flickr perhaps
    moving their albums to another server
    
    * Re-enable photo deletion

---
+++ lib/treelib/tasks/missing_albums.ex
@@ -0,0 +1,34 @@
+defmodule Treelib.Tasks.MissingAlbums do
+  alias Treelib.Taxonomy.SpeciesManager
+  alias Treelib.PhotoManager
+  alias Treelib.Repo
+
+  def process do
+    IO.puts("[MA]: Starting")
+
+    SpeciesManager.all
+    |> SpeciesManager.with_genus
+    |> Enum.each(fn s ->
+      PhotoManager.get_album(s.album_id)
+      |> case do
+        nil -> find_and_link(s)
+        pa -> IO.puts("Album #{pa.id} found.")
+      end
+    end)
+
+    IO.puts("[MA]: Done")
+  end
+
+  def find_and_link(species) do
+    name = "#{species.genus.name} #{species.name}"
+
+    IO.puts("[MA]: Searching: #{name}")
+    PhotoManager.get_album_by_name(name)
+    |> case do
+      nil -> IO.puts("--> Not found")
+      pa -> IO.puts("--> Found an album with name #{name}")
+        SpeciesManager.update_species(species, %{ album_id: pa.id })
+    end
+  end
+
+end
