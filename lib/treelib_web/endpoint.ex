99727b00c64203cc87830c9b944dba298953b707
Nathan Willson <willson.b.nathan@gmail.com>
2020-05-31 23:49:08 +0900

    Add cache control for assets
    https://hexdocs.pm/plug/Plug.Static.html#module-cache-mechanisms

--- lib/treelib_web/endpoint.ex
+++ lib/treelib_web/endpoint.ex
@@ -9,7 +9,10 @@ defmodule TreelibWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :treelib, gzip: true,
+    at: "/",
+    from: :treelib,
+    gzip: true,
+    cache_control_for_etags: "public, max-age=31536000",
     only: ~w(css webfonts fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
