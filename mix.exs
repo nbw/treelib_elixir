6a2b879ce88458424ca01ad68b6177bb45792f50
Nathan Willson <willson.b.nathan@gmail.com>
2020-11-14 10:58:03 +0900

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Treelib.Mixfile do
     [
       app: :treelib,
       version: "0.0.3",
-      elixir: "~> 1.9.4",
+      elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
