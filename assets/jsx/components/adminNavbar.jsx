25d1da394761838802e59dd896e24899d61a751f
Nathan Willson <willson.b.nathan@gmail.com>
2021-05-08 13:00:34 +0900

    rebuild

--- assets/jsx/components/adminNavbar.jsx
+++ assets/jsx/components/adminNavbar.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 
 class AdminNavbar extends React.Component {
-  refreshClick() {
-    var self = this;
-    fetch("/admin/refresh", {
-      method: "POST",
-      credentials: "same-origin",
-      headers: {
-        "X-CSRF-Token": CSRF_TOKEN,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-      .then(function (response) {
-        if (response.ok) {
-          alert("Refresh successful.");
-        } else {
-          console.log("Network response was not ok.");
-        }
-      })
-      .catch(function (error) {
-        console.log(
-          "There has been a problem with your fetch operation: " +
-            error.message,
-        );
-        alert("Response was not ok.");
-      });
-  }
   render() {
-    var self = this;
     return (
       <div id="adminNavbar">
         <div className="title">
@@ -54,9 +27,6 @@ class AdminNavbar extends React.Component {
         </div>
         <div className="item">
           <a href="/admin/debug">Debug</a>
-        </div>
-        <div className="refresh" onClick={(event) => self.refreshClick()}>
-          Refresh
         </div>
       </div>
     );
